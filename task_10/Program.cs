55a87e5bcd8234eb75bf9190553633cfb408db4a
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-25 15:20:45 +0300

    task_10

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,8 +1,13 @@
-﻿Console.Write("Введите трехзначное число");
+﻿Console.Write("Введите трехзначное число ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int num1 = num / 100;
 int num2 = (num / 10) % 10;
 int num3 = num % 10;
 
-Console.Write($"Число в середине {num2}")$
+
+if(num1 > 9 | num1 == 0)
+Console.WriteLine("ОШИБКА!!! Вы ввели не трехзначное число");
+else
+Console.Write($"Число в середине {num2}");
+
---
+++ task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 100;
+Console.WriteLine(num1);
+
+if(num1 == 0)
+Console.Write("Третьей цифры нет");
+
+int num2 = num % 100;
+Console.Write(num2);
