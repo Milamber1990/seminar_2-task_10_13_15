8ca99b8f8fd77caf9bae0f27dc57616edfff0b83
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-25 12:21:00 +0300

    Initiall commit

---
+++ task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 100;
+int num2 = (num / 10) % 10;
+int num3 = num % 10;
+
+Console.Write($"Число в середине {num2}")$
